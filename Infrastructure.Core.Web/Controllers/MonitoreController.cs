f1bf7249d930d91f167428d8ab0bb32f8b610588
kevin löw <kevin@kloew.at>
2024-04-03 14:46:41 +0200

    MonitoreController

---
+++ Infrastructure.Core.Web/Controllers/MonitoreController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using SoftwaredeveloperDotAt.Infrastructure.Core.EntityFramework;
+
+namespace SoftwaredeveloperDotAt.Infrastructure.Core.Web.Controllers
+{
+    public class MonitoreController : BaseApiController
+    {
+        private readonly IDbContext _dbContext;
+        public MonitoreController(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public bool IsAlive() => true;
+
+        [HttpGet]
+        [AllowAnonymous]
+        public bool IsDBConnected()
+        {
+            _dbContext.Database.OpenConnection();
+            return true;
+        }
+    }
+}
