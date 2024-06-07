b54991cef2fd35f754b0f293189d0022ed3c61f7
kevin löw <kevin@kloew.at>
2024-06-07 16:14:51 +0200

    JsonUtility

---
+++ Infrastructure.Core/Utility/JsonUtility.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace SoftwaredeveloperDotAt.Infrastructure.Core.Utility
+{
+    public static class JsonUtility
+    {
+        public static string ToJson<T>(this T obj, JsonSerializerSettings settings = null)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obj, settings);
+        }
+
+        public static T FromJson<T>(this string json, JsonSerializerSettings settings = null)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json, settings);
+        }
+    }
+}
