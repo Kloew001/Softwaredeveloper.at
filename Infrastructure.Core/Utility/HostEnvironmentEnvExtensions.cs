15379d3310c17118acdee2f0392e6cf59babdcf7
kevin löw <kevin@kloew.at>
2024-07-16 15:16:42 +0200

    HostEnvironmentEnvExtensions

---
+++ Infrastructure.Core/Utility/HostEnvironmentEnvExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace SoftwaredeveloperDotAt.Infrastructure.Core.Utility
+{
+    public static class HostEnvironmentEnvExtensions
+    {
+        public static bool IsTest(this IHostEnvironment hostEnvironment)
+        {
+            return hostEnvironment.IsEnvironment(Environments.Test);
+        }
+    }
+
+    public static class Environments
+    {
+        public static readonly string Test = "Test";
+    }
+}
